2fb0c89954aed320d7eda684900681665591c5be
sanjeevpatil804 <sanjeevpatil804@aol.com>
2026-06-17 11:03:23 -0400

    Funtion closer and function hoisting

---
+++ followersCount/followers_count.js
@@ -0,0 +1,21 @@
+let count = 0; //Initialize count to 0
+
+function increaseCount(){
+    count++; // Increment the count by 1
+    displayCount(); // Display ther count
+    checkCountValue(); // check count value and greet
+}
+
+function displayCount(){
+    document.getElementById('countDisplay').innerHTML = count; // Display the count in the HTML
+}
+
+function checkCountValue(){
+    if (count === 10){
+        alert("Your Instagram post gained 10 follower, Congratulations!");
+    }
+        else if (count===20){
+            alert("Your Instagram post gained 20 follower, Congratulations!");
+
+        }
+    }
